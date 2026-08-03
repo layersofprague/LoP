5bb517ccacb07f895fc35c6f6a1f3bd5c481911a
layersofprague <layersofprague@email.cz>
2026-08-03 14:12:07 +0200

    Add files via upload

--- i18n.js
+++ i18n.js
@@ -38,7 +38,7 @@ let _T = {};
 
 /* Verze v dotazu — bez ní podává GitHub Pages starou translations.json
    z cache a chybějící klíče se vypisují doslova ("overview.geo_short"). */
-const LOP_DATA_VER = '20260826';
+const LOP_DATA_VER = '20260827';
 
 (function _load() {
   try {
